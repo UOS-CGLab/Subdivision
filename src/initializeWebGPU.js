18879cc7870f77454f609cb5b2db7c9424ea7fe3
Seobeen Jang <spoof_uos@icloud.com>
2024-09-04 17:35:12 +0900

    Update initializeWebGPU.js

--- src/initializeWebGPU.js
+++ src/initializeWebGPU.js
@@ -28,12 +28,12 @@ export async function initializeWebGPU() {
 }
 
 export async function fetchData(myString) {
-    const data = await fetch('./../'+myString+'/topology.json');
-    const data2 = await fetch('./../'+myString+'/base.json');
+    const data = await fetch('./'+myString+'/topology.json'); //need to fix
+    const data2 = await fetch('./../'+myString+'/base.json'); // need to fix
     const obj = await data.json();
     const base = await data2.json();
 
-    const animationBase = await (await fetch('./../'+myString+'/animation/base.json')).json();
+    const animationBase = await (await fetch('./'+myString+'/animation/base.json')).json(); // need to fifx
 
     return { obj, base, animationBase };
 }
