c97631e3e1cf5f788c60e4b55a1861f6635d367b
seobeenJang <spoof_uos@icloud.com>
2024-10-31 12:51:50 +0900

    .

--- README.md
+++ README.md
@@ -1,23 +1,3 @@
-24.06.24 extra 해결
+2024.10.31. 코드 정리 필요
 
-근데 작동되는 것들이 대부분 level 4까지만 됩니다 이유는 모델별로 level6 topology.json을 안뽑았어요
-
-그중에서 8(suzzane)은 level6 topology고
-multifrog는 제가 잘못 덮어써서 아예 망가졌습니다
-옛날 multifrog의 topology 가지고 있으면 commit 좀 부탁해요
-
-
-24.07.09
-
-GUI 개선완료
-
-그림자 수정 해야함.
-
-https://uos-cglab.github.io/Subdivision
-
-
-24.08.04
-코드 대대적인 수정 필요
-
-https://github.com/UOS-CGLab/WebGPU-with-Cpp 중 src/Application.h, src/Applicaion2.cpp에 예시를 만들어 놓았으니 확인 부탁.
-
+limit의 depth값을 검증하는 것이 필요함.
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname)));
+
+// Default route to serve an index.html file if it exists in the current directory
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
